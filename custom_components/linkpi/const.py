50615194feaf611e413f65b6ee6240b451f2c566
breendan <59722741+breendan@users.noreply.github.com>
2025-08-21 21:59:08 +1200

    Create const.py

---
+++ custom_components/linkpi/const.py
@@ -0,0 +1,12 @@
+"""Constants for the LinkPi integration."""
+
+DOMAIN = "linkpi"
+
+# Configuration keys
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+# Defaults
+DEFAULT_SCAN_INTERVAL = 60  # seconds
